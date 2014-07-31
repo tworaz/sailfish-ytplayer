90d3e09ebea1015b4e9be6a53c312dd675e7501d
equeim <equeim@gmail.com>
2014-07-31 13:16:53 +0400

    russian translation

---
+++ languages/ru.ts
@@ -0,0 +1,382 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name></name>
+    <message id="ytplayer-error-summary">
+        <location filename="../qml/YTPlayer.qml" line="81"/>
+        <source>Internal application error</source>
+        <extracomment>Internal application error notification summary</extracomment>
+        <translation>Внутренняя ошибка</translation>
+    </message>
+    <message id="ytplayer-http-error-summary">
+        <location filename="../qml/YTPlayer.qml" line="85"/>
+        <source>HTTP error</source>
+        <extracomment>HTTP error notification summary</extracomment>
+        <translation>Ошибка HTTP</translation>
+    </message>
+    <message id="ytplayer-unknown-error-summary">
+        <location filename="../qml/YTPlayer.qml" line="89"/>
+        <source>Unknown network error</source>
+        <extracomment>Unknown HTTP error notification summary</extracomment>
+        <translation>Неизвестная ошибка сети</translation>
+    </message>
+    <message id="ytplayer-http-error-body">
+        <location filename="../qml/YTPlayer.qml" line="98"/>
+        <source>The server has returned %1</source>
+        <extracomment>Http client error notification body</extracomment>
+        <translation>Сервер возвратил %1</translation>
+    </message>
+    <message id="ytplayer-label-video-count">
+        <location filename="../qml/cover/ChannelBrowser.qml" line="85"/>
+        <location filename="../qml/pages/ChannelBrowser.qml" line="251"/>
+        <source>Video count</source>
+        <extracomment>Label for channel video count field</extracomment>
+        <translation>Число видео</translation>
+    </message>
+    <message id="ytplayer-label-network-offline">
+        <location filename="../qml/cover/NetworkOffline.qml" line="65"/>
+        <location filename="../qml/pages/NetworkOffline.qml" line="61"/>
+        <source>Network Offline</source>
+        <extracomment>Network offline screen label
+----------
+Network offline screen label</extracomment>
+        <translation>Нет сети</translation>
+    </message>
+    <message id="ytplayer-title-about">
+        <location filename="../qml/pages/About.qml" line="44"/>
+        <location filename="../qml/pages/Settings.qml" line="55"/>
+        <source>About YTPlayer</source>
+        <extracomment>Title of about page</extracomment>
+        <translation>О YTPlayer</translation>
+    </message>
+    <message id="ytplayer-label-application-description">
+        <location filename="../qml/pages/About.qml" line="58"/>
+        <source>Unofficial YouTube client for Sailfish OS</source>
+        <extracomment>YTPlayer application description in about page</extracomment>
+        <translation>Неофициальный YouTube-клиент для Sailfish OS</translation>
+    </message>
+    <message id="ytplayer-label-version">
+        <location filename="../qml/pages/About.qml" line="80"/>
+        <source>Version: %1</source>
+        <extracomment>Version label value</extracomment>
+        <translation>Версия: %1</translation>
+    </message>
+    <message id="ytplayer-label-region-code">
+        <location filename="../qml/pages/About.qml" line="89"/>
+        <source>Region code: %1</source>
+        <extracomment>Region code field value</extracomment>
+        <translation>Код региона: %1</translation>
+    </message>
+    <message id="ytplayer-title-subscribed-channels">
+        <location filename="../qml/pages/Account.qml" line="46"/>
+        <source>Subscribed Channels</source>
+        <extracomment>YouTube subscribed channels page title</extracomment>
+        <translation>Подписки</translation>
+    </message>
+    <message id="ytplayer-title-subscription-videos">
+        <location filename="../qml/pages/Account.qml" line="55"/>
+        <source>Latest Videos</source>
+        <extracomment>YouTube latest subscribed videos page title</extracomment>
+        <translation>Последние видео</translation>
+    </message>
+    <message id="ytplayer-title-likes">
+        <location filename="../qml/pages/Account.qml" line="64"/>
+        <source>Likes</source>
+        <extracomment>YouTube likes page title</extracomment>
+        <translation>Понравилось</translation>
+    </message>
+    <message id="ytplayer-title-dislikes">
+        <location filename="../qml/pages/Account.qml" line="73"/>
+        <source>Dislikes</source>
+        <extracomment>YouTube dislikes page title</extracomment>
+        <translation>Не понраивлось</translation>
+    </message>
+    <message id="ytplayer-title-recommended">
+        <location filename="../qml/pages/Account.qml" line="82"/>
+        <source>Recommended for you</source>
+        <extracomment>YouTube recommendations page title</extracomment>
+        <translation>Рекомендованное</translation>
+    </message>
+    <message id="ytplayer-channel-unsubscribe">
+        <location filename="../qml/pages/ChannelBrowser.qml" line="149"/>
+        <source>Unsubscribe</source>
+        <extracomment>Menu option to unsubscribe from YouTube channel</extracomment>
+        <translation>Отписаться</translation>
+    </message>
+    <message id="ytplayer-channel-subscribe">
+        <location filename="../qml/pages/ChannelBrowser.qml" line="152"/>
+        <source>Subscribe</source>
+        <translation>Подписаться</translation>
+    </message>
+    <message id="ytplayer-label-created-on">
+        <location filename="../qml/pages/ChannelBrowser.qml" line="241"/>
+        <source>Created on</source>
+        <extracomment>Label for youtube channel creation date field</extracomment>
+        <translation>Создан</translation>
+    </message>
+    <message id="ytplayer-label-latest-videos">
+        <location filename="../qml/pages/ChannelBrowser.qml" line="283"/>
+        <source>Latest videos</source>
+        <extracomment>Label/Title for the list of latest videos in certain category</extracomment>
+        <translation>Последние</translation>
+    </message>
+    <message id="ytplayer-label-loading">
+        <location filename="../qml/pages/LoadingScreen.qml" line="81"/>
+        <source>Loading</source>
+        <extracomment>Generic label informing user some content is being loaded</extracomment>
+        <translation>Загрузка</translation>
+    </message>
+    <message id="ytplayer-title-log-viewer">
+        <location filename="../qml/pages/LogViewer.qml" line="46"/>
+        <source>Log Viewer</source>
+        <translation></translation>
+    </message>
+    <message id="ytplayer-action-search">
+        <location filename="../qml/pages/MainMenu.qml" line="89"/>
+        <source>Search</source>
+        <extracomment>Menu option to show search page</extracomment>
+        <translation>Поиск</translation>
+    </message>
+    <message id="ytplayer-action-video-categories">
+        <location filename="../qml/pages/MainMenu.qml" line="97"/>
+        <source>Video Categories</source>
+        <extracomment>Menu option to show video categories page</extracomment>
+        <translation>Категории</translation>
+    </message>
+    <message id="ytplayer-label-account">
+        <location filename="../qml/pages/MainMenu.qml" line="105"/>
+        <location filename="../qml/pages/Settings.qml" line="74"/>
+        <source>Account</source>
+        <extracomment>Label for YouTube account related options in main menu
+----------
+Account settings section label</extracomment>
+        <translation>Аккаунт</translation>
+    </message>
+    <message id="ytplayer-action-recommended">
+        <location filename="../qml/pages/MainMenu.qml" line="116"/>
+        <source>Recommended for you</source>
+        <extracomment>Menu option fo show YouTube recommendations page</extracomment>
+        <translation>Рекомендованное</translation>
+    </message>
+    <message id="ytplayer-action-subscriptions">
+        <location filename="../qml/pages/MainMenu.qml" line="126"/>
+        <source>Subscriptions</source>
+        <extracomment>Menu option responsible for showing user subscriptions page</extracomment>
+        <translation>Подписки</translation>
+    </message>
+    <message id="ytplayer-action-latest-subscribed-videos">
+        <location filename="../qml/pages/MainMenu.qml" line="134"/>
+        <source>Latest videos</source>
+        <extracomment>Sub-Menu option responsible for showing latest subsribed videos page</extracomment>
+        <translation>Последние</translation>
+    </message>
+    <message id="ytplayer-action-subscribed-channels">
+        <location filename="../qml/pages/MainMenu.qml" line="141"/>
+        <source>Channels</source>
+        <extracomment>Sub-Menu option responsible for showing user subscribed channels</extracomment>
+        <translation>Каналы</translation>
+    </message>
+    <message id="ytplayer-action-likes">
+        <location filename="../qml/pages/MainMenu.qml" line="153"/>
+        <source>Likes</source>
+        <extracomment>Menu option responsible for showing user likes page</extracomment>
+        <translation>Понравилось</translation>
+    </message>
+    <message id="ytplayer-action-dislikes">
+        <location filename="../qml/pages/MainMenu.qml" line="163"/>
+        <source>Dislikes</source>
+        <extracomment>Menu option responsible for showing user dislikes page</extracomment>
+        <translation>Не понравилось</translation>
+    </message>
+    <message id="ytplayer-action-other">
+        <location filename="../qml/pages/MainMenu.qml" line="173"/>
+        <source>Other</source>
+        <extracomment>Label for misc items in main application menu</extracomment>
+        <translation>Другое</translation>
+    </message>
+    <message id="ytplayer-action-settings">
+        <location filename="../qml/pages/MainMenu.qml" line="182"/>
+        <source>Settings</source>
+        <extracomment>Menu option to show settings page</extracomment>
+        <translation>Настройки</translation>
+    </message>
+    <message id="ytplayer-label-network-connection-retry">
+        <location filename="../qml/pages/NetworkOffline.qml" line="74"/>
+        <source>Retry</source>
+        <extracomment>Label of network connection retry button</extracomment>
+        <translation>Повторить</translation>
+    </message>
+    <message id="ytplayer-label-search">
+        <location filename="../qml/pages/Search.qml" line="110"/>
+        <location filename="../qml/pages/Settings.qml" line="105"/>
+        <source>Search</source>
+        <extracomment>Label of video search text field
+----------
+Search settings section label</extracomment>
+        <translation>Поиск</translation>
+    </message>
+    <message id="ytplayer-search-no-results">
+        <location filename="../qml/pages/Search.qml" line="123"/>
+        <source>No results</source>
+        <extracomment>Background label informing the user there are no search results</extracomment>
+        <translation>Ничего не найдено</translation>
+    </message>
+    <message id="ytplayer-title-show-logs">
+        <location filename="../qml/pages/Settings.qml" line="49"/>
+        <source>View Logs</source>
+        <translation></translation>
+    </message>
+    <message id="ytplayer-title-settings">
+        <location filename="../qml/pages/Settings.qml" line="68"/>
+        <source>Settings</source>
+        <extracomment>Settings page title</extracomment>
+        <translation>Настройки</translation>
+    </message>
+    <message id="ytplayer-account-integration-label">
+        <location filename="../qml/pages/Settings.qml" line="84"/>
+        <source>YouTube account integration</source>
+        <extracomment>Label of switch activating/deactivating YouTube account integration</extracomment>
+        <translation>Интеграция с аккаунтом YouTube</translation>
+    </message>
+    <message id="ytplayer-account-integration-description">
+        <location filename="../qml/pages/Settings.qml" line="87"/>
+        <source>Allow YTPlayer to manage YouTube user account</source>
+        <extracomment>Description of switch activating/deactivating YouTube account integration</extracomment>
+        <translation>Разрешить YTPlayer управлять YouTube-аккаунтом</translation>
+    </message>
+    <message id="ytplayer-label-content-filtering">
+        <location filename="../qml/pages/Settings.qml" line="114"/>
+        <source>Content filtering</source>
+        <extracomment>Content filtering settings option label</extracomment>
+        <translation>Фильтрация контента</translation>
+    </message>
+    <message id="ytplayer-content-fitering-none">
+        <location filename="../qml/pages/Settings.qml" line="122"/>
+        <source>None</source>
+        <extracomment>Option value for lack of any content filtering</extracomment>
+        <translation>Нет</translation>
+    </message>
+    <message id="ytplayer-content-filtering-moderate">
+        <location filename="../qml/pages/Settings.qml" line="127"/>
+        <source>Moderate</source>
+        <extracomment>Option value for moderate content filtering</extracomment>
+        <translation>Умеренная</translation>
+    </message>
+    <message id="ytplayer-content-filtering-strict">
+        <location filename="../qml/pages/Settings.qml" line="132"/>
+        <source>Strict</source>
+        <extracomment>Option value for strict content filtering</extracomment>
+        <translation>Строгая</translation>
+    </message>
+    <message id="ytplayer-label-display">
+        <location filename="../qml/pages/Settings.qml" line="142"/>
+        <source>Display</source>
+        <extracomment>Display settings section label</extracomment>
+        <translation>Отображение</translation>
+    </message>
+    <message id="ytplayer-label-results-per-page">
+        <location filename="../qml/pages/Settings.qml" line="157"/>
+        <source>Results per page</source>
+        <extracomment>Label of results per page slider in display settings menu</extracomment>
+        <translation>Результаты на страницу</translation>
+    </message>
+    <message id="ytplayer-title-video-categories">
+        <location filename="../qml/pages/VideoCategories.qml" line="77"/>
+        <source>Video Categories</source>
+        <extracomment>Video categories page title</extracomment>
+        <translation>Категории</translation>
+    </message>
+    <message id="ytplayer-label-video-quality">
+        <location filename="../qml/pages/VideoController.qml" line="129"/>
+        <source>Video quality</source>
+        <extracomment>Label for menu option allowing the user to change video quality</extracomment>
+        <translation>Качество видео</translation>
+    </message>
+    <message id="ytplayer-status-buffering">
+        <location filename="../qml/pages/VideoController.qml" line="296"/>
+        <location filename="../qml/pages/VideoController.qml" line="317"/>
+        <source>Buffering: %1%</source>
+        <extracomment>Media player status indicating content is buffering</extracomment>
+        <translation>Буферизация: %1%</translation>
+    </message>
+    <message id="ytplayer-status-no-media">
+        <location filename="../qml/pages/VideoController.qml" line="307"/>
+        <source>No media</source>
+        <extracomment>Media player status indicating there is no content to play</extracomment>
+        <translation>Нет видео</translation>
+    </message>
+    <message id="ytplayer-status-loading">
+        <location filename="../qml/pages/VideoController.qml" line="310"/>
+        <source>Loading</source>
+        <extracomment>Media player status indicating content is loading</extracomment>
+        <translation>Загрузка</translation>
+    </message>
+    <message id="ytplayer-status-loaded">
+        <location filename="../qml/pages/VideoController.qml" line="313"/>
+        <source>Loaded</source>
+        <extracomment>Media player status indicating content was loaded</extracomment>
+        <translation>Загружено</translation>
+    </message>
+    <message id="ytplayer-status-stalled">
+        <location filename="../qml/pages/VideoController.qml" line="321"/>
+        <source>Stalled</source>
+        <extracomment>Media player status indicating content loading has stalled</extracomment>
+        <translation>Остановлено</translation>
+    </message>
+    <message id="ytplayer-status-buffered">
+        <location filename="../qml/pages/VideoController.qml" line="324"/>
+        <source>Buffered</source>
+        <extracomment>Media player status indicating content has been buffered</extracomment>
+        <translation>Буферизовано</translation>
+    </message>
+    <message id="ytplayer-status-end-of-media">
+        <location filename="../qml/pages/VideoController.qml" line="327"/>
+        <source>End of media</source>
+        <extracomment>Media player status indicating end of content has been reached</extracomment>
+        <translation>Конец</translation>
+    </message>
+    <message id="ytplayer-status-invalid-media">
+        <location filename="../qml/pages/VideoController.qml" line="330"/>
+        <source>Invalid media</source>
+        <extracomment>Media player status indicating invalid content type</extracomment>
+        <translation>Недопустимый контент</translation>
+    </message>
+    <message id="ytplayer-label-publish-date">
+        <location filename="../qml/pages/VideoOverview.qml" line="157"/>
+        <source>Published on</source>
+        <extracomment>Label for video upload date field</extracomment>
+        <translation>Опубликовано</translation>
+    </message>
+    <message id="ytplayer-label-duration">
+        <location filename="../qml/pages/VideoOverview.qml" line="167"/>
+        <source>Duration</source>
+        <extracomment>Label for video duration field</extracomment>
+        <translation>Длительность</translation>
+    </message>
+    <message id="ytplayer-youtube-sign-in-successful">
+        <location filename="../qml/pages/YTOAuth2.qml" line="62"/>
+        <source>Sign in successful</source>
+        <extracomment>Notification informing the user that YouTube sign in succeeded</extracomment>
+        <translation>Вход успешен</translation>
+    </message>
+    <message id="ytplayer-oauth-failed">
+        <location filename="../qml/pages/YTOAuth2.qml" line="87"/>
+        <source>OAuth authorization failed!</source>
+        <extracomment>Error message informing the user about OAuth authorization failure</extracomment>
+        <translation>Авторизация OAuth не удалась!</translation>
+    </message>
+    <message id="ytplayer-oauth-page-loading-failed">
+        <location filename="../qml/pages/YTOAuth2.qml" line="117"/>
+        <source>Failed to load OAuth authorization page!</source>
+        <extracomment>YouTube OAuth page loading failure message</extracomment>
+        <translation>Не удалось загрузить страницу авторизации!</translation>
+    </message>
+    <message id="ytplayer-oauth-access-denied">
+        <location filename="../qml/pages/YTOAuth2.qml" line="132"/>
+        <source>YouTube OAuth access denined!</source>
+        <extracomment>Message informing the user about YouTube OAuth autorization denial</extracomment>
+        <translation>YouTube OAuth авторизация отклонена!</translation>
+    </message>
+</context>
+</TS>
