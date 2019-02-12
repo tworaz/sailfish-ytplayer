ab4272e68c75f1f8c3413562979e7128206a6b55
Matti Viljanen <matti.viljanen@kapsi.fi>
2019-02-12 14:25:06 +0200

    Don't use git; it doesn't seem to work on shadow builds

--- scripts/get_version_str.sh
+++ scripts/get_version_str.sh
@@ -4,10 +4,6 @@ _SCRIPT=$(readlink -f $0)
 _DIR=$(dirname $_SCRIPT)/..
 
 _BASE=$(grep -oE 'Version:[[:space:]]*[0-9.]*' $_DIR/rpm/harbour-ytplayer.spec | awk '{ print $2 }')
+_REV=$(grep -oE 'Revision:[[:space:]]*[0-9.]*' $_DIR/rpm/harbour-ytplayer.spec | awk '{ print $2 }')
 
-if [ -d $_DIR/.git ]; then
-	_REV=$(git rev-parse --short HEAD)
-	echo "$_BASE-$_REV"
-else
-	echo $_BASE
-fi
+echo "$_BASE-$_REV"
