bcf2197c2625f656c8f17d67809668cfa6d975b8
Matti Viljanen <matti.viljanen@kapsi.fi>
2019-05-20 00:19:17 +0300

    Fix version parsing

--- scripts/get_version_str.sh
+++ scripts/get_version_str.sh
@@ -4,6 +4,6 @@ _SCRIPT=$(readlink -f $0)
 _DIR=$(dirname $_SCRIPT)/..
 
 _BASE=$(grep -oE 'Version:[[:space:]]*[0-9.]*' $_DIR/rpm/harbour-ytplayer.spec | awk '{ print $2 }')
-_REV=$(grep -oE 'Revision:[[:space:]]*[0-9.]*' $_DIR/rpm/harbour-ytplayer.spec | awk '{ print $2 }')
+_REV=$(grep -oE 'Release:[[:space:]]*[0-9.]*' $_DIR/rpm/harbour-ytplayer.spec | awk '{ print $2 }')
 
 echo "$_BASE-$_REV"
