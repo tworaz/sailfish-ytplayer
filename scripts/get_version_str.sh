3e913ac3c9b84ff0f4a5dc42a3583330e104b684
Matti Viljanen <matti.viljanen@kapsi.fi>
2019-09-25 00:46:03 +0300

    Fix git head revision number

--- scripts/get_version_str.sh
+++ scripts/get_version_str.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
+_BASE=$(grep -oE 'Version:[[:space:]]*[0-9.]*' rpm/harbour-ytplayer.spec | awk '{ print $2 }')
+_REV=$(grep -oE 'Release:[[:space:]]*[0-9.]*' rpm/harbour-ytplayer.spec | awk '{ print $2 }')
 
-_SCRIPT=$(readlink -f $0)
-_DIR=$(dirname $_SCRIPT)/..
-
-_BASE=$(grep -oE 'Version:[[:space:]]*[0-9.]*' $_DIR/rpm/harbour-ytplayer.spec | awk '{ print $2 }')
-_REV=$(grep -oE 'Release:[[:space:]]*[0-9.]*' $_DIR/rpm/harbour-ytplayer.spec | awk '{ print $2 }')
+if [ -d .git ]; then
+    _REV=$(git rev-parse --short HEAD)
+fi
 
 echo "$_BASE-$_REV"
